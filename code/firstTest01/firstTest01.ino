85db3c9365964ab104c7f1151c39c04411a754f3
Linus <hoppelinus@web.de>
2023-12-01 16:20:33 +0100

    Tested calculateRollingAverage Function
    Verified that the calculateRollingAverage function is working correctly during testing. However, in our specific use case, it appears impractical as we require detection of values surpassing the threshold.

--- code/firstTest01/firstTest01.ino
+++ code/firstTest01/firstTest01.ino
@@ -10,9 +10,32 @@
 const int THRESHOLD_MA = 900; 
 const int THRESHOLD_MIN = 550;
 
-// 
-int global_counter = 0; 
-const 
+
+
+const int bufferSize = 10;  // Size of the rolling average buffer
+int sensorValues[bufferSize];  // Array for storing sensor values
+
+int calculateRollingAverage(int newValue) {
+  static int index = 0;  // Current index in the array
+  static int sum = 0;    // Sum of values in the array
+
+  // Subtract the oldest element from the sum
+  sum -= sensorValues[index];
+
+  // Add the new element to the sum
+  sum += newValue;
+
+  // Store the new element in the array
+  sensorValues[index] = newValue;
+
+  // Increment the index and set it to 0 if it reaches the end of the array
+  index = (index + 1) % bufferSize;
+
+  // Calculate the rolling average
+  int average = sum / bufferSize;
+
+  return average;
+}
 
 
 void setup() {
@@ -39,9 +62,11 @@ void loop() {
       Serial.print(" ");
       Serial.print(1200); // To freeze the upper limit
       Serial.print(" ");
-      Serial.println(analogRead(OUTPUT_PIN));  // To send all three 'data' points to the plotter
+      // Calculate the rolling average
+      int rollingAverage = calculateRollingAverage(analogRead(OUTPUT_PIN));
+      Serial.println(rollingAverage);  // To send all three 'data' points to the plotter
   }
   //Wait for a bit to keep serial data from saturating
-  delay(10);
+  delay(1);
 } 
 
